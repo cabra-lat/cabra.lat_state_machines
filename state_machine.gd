6517c7f1811e88b90fda34ea78102413300a9e3b
@cabra.lat <admin@cabra.lat>
2025-10-04 09:30:51 -0300

    added package

--- state_machine.gd
+++ state_machine.gd
@@ -2,6 +2,8 @@
 class_name StateMachine extends AnimationTree
 
 signal state_changed(new_state: String, old_state: String)
+signal state_entered(state: String)
+signal state_exited(state: String)
 
 var _last_state: String = ""
 
@@ -11,7 +13,7 @@ func _ready():
 
 func _set_transitions_to_auto(sm: AnimationNodeStateMachine) -> void:
 	if sm == null: return
-	for i in range(sm.get_transition_count()):
+	for i in sm.get_transition_count():
 		var trans = sm.get_transition(i)
 		if trans:
 			trans.advance_mode = AnimationNodeStateMachineTransition.ADVANCE_MODE_AUTO
@@ -23,8 +25,10 @@ func get_condition(name: String):
 	return self.get("parameters/conditions/" + name)
 
 func get_current_state() -> String:
-	var current = self.get("parameters/playback").get_current_node()
+	var current = get("parameters/playback").get_current_node()
 	if current != _last_state:
+		emit_signal("state_exited", _last_state)
 		emit_signal("state_changed", current, _last_state)
+		emit_signal("state_entered", current)
 		_last_state = current if current else "Start"
 	return current if current else "Start"
