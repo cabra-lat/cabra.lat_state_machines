c48003f5df77844bb0c50229ba2104a06abcd768
@cabra.lat <admin@cabra.lat>
2025-10-28 03:12:11 -0300

    updoot

--- state_machine.gd
+++ state_machine.gd
@@ -5,30 +5,32 @@ signal state_changed(new_state: String, old_state: String)
 signal state_entered(state: String)
 signal state_exited(state: String)
 
-var _last_state: String = ""
+var state: String:
+  get: return get("parameters/playback").get_current_node()
+var _last_state: String = "Start"
 
 func _ready():
-	if not Engine.is_editor_hint():
-		_set_transitions_to_auto(tree_root)
+  if not Engine.is_editor_hint():
+    _set_transitions_to_auto(tree_root)
 
 func _set_transitions_to_auto(sm: AnimationNodeStateMachine) -> void:
-	if sm == null: return
-	for i in sm.get_transition_count():
-		var trans = sm.get_transition(i)
-		if trans:
-			trans.advance_mode = AnimationNodeStateMachineTransition.ADVANCE_MODE_AUTO
+  if sm == null: return
+  for i in sm.get_transition_count():
+    var trans = sm.get_transition(i)
+    if trans:
+      trans.advance_mode = AnimationNodeStateMachineTransition.ADVANCE_MODE_AUTO
 
 func set_condition(name: String, value):
-	self["parameters/conditions/" + name] = value
+  self.set("parameters/conditions/" + name, value)
 
 func get_condition(name: String):
-	return self.get("parameters/conditions/" + name)
+  return self.get("parameters/conditions/" + name)
 
 func get_current_state() -> String:
-	var current = get("parameters/playback").get_current_node()
-	if current != _last_state:
-		emit_signal("state_exited", _last_state)
-		emit_signal("state_changed", current, _last_state)
-		emit_signal("state_entered", current)
-		_last_state = current if current else "Start"
-	return current if current else "Start"
+  var current = get("parameters/playback").get_current_node()
+  if current != _last_state:
+    emit_signal("state_exited", _last_state)
+    emit_signal("state_changed", current, _last_state)
+    emit_signal("state_entered", current)
+    _last_state = current if current else "Start"
+  return current if current else "Start"
